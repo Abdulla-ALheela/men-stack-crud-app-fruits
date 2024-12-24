366eab53911d64fcf849b0a858d710950a9c442a
Abdulla Alheela <Abdulla.alheela@gmail.com>
2024-12-24 20:35:54 +0300

    app completed

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ require("./config/database")
 //----------------------------------Import the Packages------------------------------------------//
 
 const express = require('express');
+const methodOverride = require("method-override");
 const morgan = require('morgan');
+const path = require("path");
 
 //Models
 const Fruit = require("./models/fruit.js");
@@ -15,8 +17,9 @@ const app = express();
 //-------------------------------------Middlewares------------------------------------------------//
 
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 app.use(morgan('dev'));// Use Morgan middleware with the 'dev' option for concise output
-
+app.use(express.static(path.join(__dirname, "public")));
 //----------------------------------------Routes--------------------------------------------------//
 
 app.get("/", async (req, res) => {
@@ -38,9 +41,47 @@ app.get("/fruits", async (req, res) => {
     res.redirect("/fruits"); 
   });
 
+ 
 app.get("/fruits/new", (req, res) => {
     res.render("fruits/new.ejs");
 });
+
+app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", { fruit: foundFruit });
+  });
+  
+  app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+  });
+
+  // server.js
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
+  });
+  
+
+  app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/edit.ejs", {
+      fruit: foundFruit,
+    });
+  });
+  
+  
 
 //----------------------------------Port 3000 Listener-------------------------------------------//
 
